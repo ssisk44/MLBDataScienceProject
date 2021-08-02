a796f1cac0a50154da9e8617785d5bed91b47eee
ssisk44 <65787613+ssisk44@users.noreply.github.com>
2021-08-01 22:38:08 -0400

    API date fix

--- WebscrapeStats/apiEndpointFunctions.py
+++ WebscrapeStats/apiEndpointFunctions.py
@@ -1,5 +1,6 @@
 import requests
 from datetime import date
+import datetime
 import calendar
 import pandas as pd
 from pandas.tests.extension.json import JSONArray
@@ -7,32 +8,27 @@ from pandas.tests.extension.json import JSONArray
 TeamsAbbreviations = ['ARI','ATL','BAL','BOS','CHC','CHW','CIN','CLE','COL','DET','HOU',"KCR","LAA","LAD",'MAI','MIL','MIN','NYM','NYY','OAK','PHI','PIT',"SDP",'SFG','SEA','STL','TBD','TEX','TOR',"WSN"]
 API_KEY = "86b4aafa44974957949c2312482b0f27"
 
-def getDate(date):
-    today = date
-    Month = calendar.month_name[today.month]
-    MonthAbbrev = str(Month[0:3].upper())
-    Year = today.year
-    Day = today.day
-    date = (str(Year) + "-" + str(MonthAbbrev) + "-" + str(Day))
-    return date
+
+def main():
+    # print("https://api.sportsdata.io/v3/mlb/scores/json/GamesByDate/"+getTodaysDate()+"?"+API_KEY+"")
+    getGamesbyDate()
+
+def getTodaysDate():
+    CalendarDict = {"01": "Jan", "02": "Feb", "03": "Mar", "04": "Apr", "05": "May", "06": "Jun", "07": "Jul",
+                    "08": "Aug", "09": "Sep", "10": "Oct", "11": "Nov", "12": "Dec"}
+    dateT = str(datetime.datetime.now().date())
+    p1 = dateT[:5]
+    p2 = dateT[7:]
+    monthAbrev = CalendarDict[dateT[5:7]]
+    return(p1 + "" + monthAbrev + "" + p2)
+
 
 def getGamesbyDate():
+    response = requests.get("https://api.sportsdata.io/v3/mlb/scores/json/GamesByDate/"+getTodaysDate()+"?key="+API_KEY+"")
+    data = response.json()
+    dfItem = pd.DataFrame.from_records(data)
+    dfItem.to_csv(r''+str(getTodaysDate())+'Games.csv', index=False)
 
-    print(("https://api.sportsdata.io/v3/mlb/scores/xml/GamesByDate/{"+getDate(date.today())+"}?"+API_KEY))
-    # response = requests.get("https://api.sportsdata.io/v3/mlb/scores/xml/GamesByDate/{"+getDate(date.today())+"}?"+key+"")
-    # print(response.status_code)
-    # print(response)
-
-# string = str("https://api.sportsdata.io/v3/mlb/scores/json/Stadiums?"+API_KEY)
-# response = requests.get("https://api.sportsdata.io/v3/mlb/scores/json/Stadiums?"+API_KEY)
-# print(response.status_code)
-# print(response)
-#
-# print(string)
-# print(response.status_code)
-
-
-# https://api.sportsdata.io/v3/mlb/scores/json/Stadiums?key=86b4aafa44974957949c2312482b0f27
 def getGamesInProgress():
     response = requests.get("https://api.sportsdata.io/v3/mlb/scores/json/AreAnyGamesInProgress?key=86b4aafa44974957949c2312482b0f27")
     print(response.status_code)
@@ -57,3 +53,6 @@ def getTeamStatsbySeason(year):
 
 def getBvP(batter,pitcher):
     "https://api.sportsdata.io/v3/mlb/stats/json/HitterVsPitcher/%7Bhitterid%7D/%7Bpitcherid%7D?key=86b4aafa44974957949c2312482b0f27"
+
+
+main()
