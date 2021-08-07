72b84605d746bd2ec3e3e84a9fd83d0f6737a4fb
ssisk44 <65787613+ssisk44@users.noreply.github.com>
2021-08-07 18:22:41 -0400

    baseball reference scrape complete, parsing needed hopefully removed need for API with this!!!!!!!!!!!!
    BASEBALL REFERENCE SCRAPE TO DO:
    - target values needed for projects
    - ammend scrappers accordingly
    - find solution to universal parse scrape output (first header value, num values)
    
    LINEUP SIM TO DO:
    - away and home lineups for batting and baserunning
    - fill out math for the rest of the functions
    - implement functions
    - configure simulation results data
    - begin testing simulations
    
    NOTE: Make sure to make these functions reusable as possible for future use

---
+++ TestEnvironment/fangraphsSCRAPE.py
@@ -0,0 +1,59 @@
+from bs4 import BeautifulSoup
+import requests
+import pandas as pd
+from selenium import webdriver
+from selenium.webdriver import ActionChains
+import numpy as np
+import time, os
+
+
+def main():
+    getPlayer()
+
+def test():
+    df = pd.read_csv('bryceharper_standard_batting.csv').to_numpy()
+    print(df)
+
+
+
+def get_csv_bbref(link_list):
+    chromedriver = "C:/Users/samue/OneDrive/Documents/chromedriver.exe"
+    os.environ["webdriver.chrome.driver"] = chromedriver
+
+    soup_list = []
+    for i in link_list:
+        driver = webdriver.Chrome(chromedriver)
+        driver.maximize_window()
+        driver.get(i)
+        time.sleep(5)
+        driver.execute_script("window.scrollTo(0, 500);")
+        e1 = driver.find_element_by_xpath('//*[@id="all_batting_standard"]/div[1]/div/ul/li[3]')
+        e2 = driver.find_element_by_xpath('//*[@id="batting_standard_sh"]/div/ul/li[3]/div/ul/li[4]/button')
+        time.sleep(2)
+
+        action = ActionChains(driver)
+        action.move_to_element(e1).perform()
+        action.move_to_element(e2).click().perform()
+
+        soup = BeautifulSoup(driver.page_source, 'html.parser')
+        crude = soup.find('pre', id='csv_batting_standard')
+        soup_list.append(crude)
+        driver.close()
+    return soup_list
+
+
+def getPlayer():
+    bharperstats_list = ["https://www.baseball-reference.com/players/h/harpebr03.shtml"]
+    raw_csv_data_list = get_csv_bbref(bharperstats_list)
+    print(raw_csv_data_list)
+
+    ############# PARSE THIS CANCER #############
+    df = pd.DataFrame(raw_csv_data_list)
+    df.to_csv(r'bryceharper_standard_batting.csv', index=False)
+
+
+
+
+main()
+
+
---
+++ TestEnvironment/pybaseballTEST.py
@@ -0,0 +1,8 @@
+from pybaseball import *
+
+#####https://github.com/jldbc/pybaseball/tree/master/docs#####
+
+# pybaseball.batting()
+data = statcast_batter('2008-04-01', '2010-07-15', player_id = 120074)
+
+data.to_csv(r'batting.csv', index=False)
