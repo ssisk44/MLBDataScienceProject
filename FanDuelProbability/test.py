20a0f506a89d5362e746a9a496d0c85f306b8402
ssisk44 <65787613+ssisk44@users.noreply.github.com>
2021-08-10 21:45:45 -0400

    ************ NEED TO AUTOMATE AND ALLOW FOR SPECIFICATION OF DATA RETREIVAL **********************
    BASEBALL REFERENCE SCRAPE TO DO:
    - target values needed for projects
    - ammend scrappers accordingly
    X find solution to universal parse scrape output (first header value, num values)
    
    LINEUP SIM TO DO:
    - away and home lineups for batting and baserunning
    - fill out math for the rest of the functions
    - implement functions
    - configure simulation results data
    - begin testing simulations
    
    FANTASY DFS COMBINATIONS AND REPORTING TO DO:
    - automate system
    - for each team in box score
    X prevent "special letters" from preventing data reporting
    
    NOTE: Make sure to make these functions reusable as possible for future use

--- FanDuelProbability/test.py
+++ FanDuelProbability/test.py
@@ -4,25 +4,31 @@ import requests, time
 import seaborn as sns
 import matplotlib.pyplot as plt
 
+file = 'ContestCSV/08092021MIASDP.csv'
 def main():
-    seabornScatterplot()
+    ###PREGAME
+    # createCombinationsFromCSV()
+    # player_name_rank_counter()
+
+    #POSTGAME
+    # getBoxScores(time.time())
+    # parseBoxScoretoPointsPerPlayer()
+    # addPlayerScoretoCSV()
+    editCombinationsWITHPlayerScores()
+
+
+
+
 
 
 ### BEFORE GAME BEGINS
-def getBoxScores(date):
-    date = date
-    response = requests.get("https://api.sportsdata.io/v3/mlb/stats/json/BoxScores/2021-Aug-09?key=86b4aafa44974957949c2312482b0f27")
-    data = response.json()
-    dfItem = pd.DataFrame.from_records(data)
-    dfItem.to_csv(r'Aug92021BoxScores.csv', index=False)
-
 def createCombinationsFromCSV():
-    array = pd.read_csv('pl.csv').to_numpy()
+    array = pd.read_csv(file).to_numpy()
     newarray = []
     for player in range(0, len(array)):
         if (str(array[player][11]) == 'nan') and (int(array[player][15]) > 0):
             newarray.append(array[player])
-
+    pd.DataFrame(newarray).to_csv(str(file[0:12]) + "_reduced_players_list.csv", index=False)
     combos = list(permutations(newarray, 5))
     permutationsarr = []
     currentlineup = []
@@ -36,11 +42,11 @@ def createCombinationsFromCSV():
                 currentlineup.append(combos[combination][player][3])  # add player to current
                 salary += combos[combination][player][7]  # add salary
                 if player == 0:
-                    lineupscore += combos[combination][player][5] * 2.5  # MVP
+                    lineupscore += float(combos[combination][player][5]) * 2.5  # MVP
                 elif player == 1:
-                    lineupscore += combos[combination][player][5] * 1.5  # STAR
+                    lineupscore += float(combos[combination][player][5]) * 1.5  # STAR
                 else:
-                    lineupscore += combos[combination][player][5]  # NORMAL
+                    lineupscore += float(combos[combination][player][5])  # NORMAL
             else:
                 break
 
@@ -53,21 +59,63 @@ def createCombinationsFromCSV():
         lineupscore = 0
 
     permutationsarr.sort(key=lambda x: x[6], reverse=True)
-    pd.DataFrame(permutationsarr).to_csv("permutations.csv", index=False)  # THIS IS FOR PERMUTATION TESTING
 
+    newpermutationsarr = [] #removes the repition of every six entries being the same
+    for i in range(6, len(permutationsarr)+6):
+        if i%6==0 and permutationsarr[i-6][5]>=35000 and permutationsarr[i-6][6]>=60: #every sixth and lineup cost above 30000
+            newpermutationsarr.append(permutationsarr[i-6])
+
+
+
+
+    pd.DataFrame(newpermutationsarr).to_csv(str(file[0:12])+"permutations.csv", index=False)  # THIS IS FOR PERMUTATION TESTING
+
+def player_name_rank_counter():
+    permutations_array = pd.read_csv(str(file[0:12])+'permutations.csv').to_numpy()
+    reduced_players_array = pd.read_csv(str(file[0:12]) + "_reduced_players_list.csv").to_numpy()
+    player_counter_array = []
+    headers = ['Player Name', "# of MVPs", "# of All Stars", "# of Regulars"]
+
+
+
+    for name in range(0,len(reduced_players_array)):
+        player_counter_array.append([reduced_players_array[name][3],0,0,0])
+
+
+    for i in range(0, len(permutations_array)):
+        for j in range(0, len(player_counter_array)):
+            for k in range(0, 5):
+
+                if permutations_array[i][k] == player_counter_array[j][0]:
+                    if k==0:
+                        player_counter_array[j][1] += 1
+                    if k==1:
+                        player_counter_array[j][2] += 2
+                    if k==2 or k==3 or k==4:
+                        player_counter_array[j][3] += 3
+
+    pd.DataFrame(player_counter_array).to_csv(str(file[0:12]) + "player_lineup_disctribution.csv",
+                                            index=False, header=headers)  # THIS IS FOR PERMUTATION TESTING
 
 
 
 ### AFTER GAME END
+def getBoxScores(date):
+    date = date
+    response = requests.get("https://api.sportsdata.io/v3/mlb/stats/json/BoxScores/2021-Aug-09?key=86b4aafa44974957949c2312482b0f27")
+    data = response.json()
+    dfItem = pd.DataFrame.from_records(data)
+    dfItem.to_csv(r'BoxScores.csv', index=False)
+
 def parseBoxScoretoPointsPerPlayer():
-    array = pd.read_csv('Aug92021BoxScores.csv').to_numpy()
+    array = pd.read_csv('BoxScores.csv').to_numpy()
     newarr = eval(array[4][3])
     playerstats = pd.DataFrame(newarr)
     # columnslist = playerstats.columns
     # for i in columnslist:
     #     print(i)
-    pd.DataFrame(newarr).to_csv(r'Aug92021PlayerStats.csv', index=False)
-    newarr = pd.read_csv('Aug92021PlayerStats.csv').to_numpy()
+    pd.DataFrame(newarr).to_csv(r'PlayerStats.csv', index=False)
+    newarr = pd.read_csv('PlayerStats.csv').to_numpy()
 
     # SCORING FOR ONE GAME DFS: MVP, ALL-STAR, 3 NORMAL
     ### 1B = 3    2B = 6    3B = 9    HR = 12   BB = 3   HBP = 3   R = 3.2   RBI = 3.5   SB = 6
@@ -92,19 +140,19 @@ def parseBoxScoretoPointsPerPlayer():
         playersscorescheck.append([newarr[i][5], round(newarr[i][42]/1.4), round(newarr[i][43]/1.4), round(newarr[i][44]/1.4), round(newarr[i][45]/1.4), round(newarr[i][50]/1.4), round(newarr[i][51]/1.4), round(newarr[i][40]/1.4), round(newarr[i][46]/1.4), round(newarr[i][55]/1.4)])
         playerandscores.append([player, score])
     playersscorescheck.sort(key=lambda x: x[2], reverse=True)
-    pd.DataFrame(playersscorescheck).to_csv(r'Aug92021PlayerScoresCheck.csv', index=False, header=psc_headers)
-    pd.DataFrame(playerandscores).to_csv(r'Aug92021PlayerScores.csv', index=False)
+    pd.DataFrame(playersscorescheck).to_csv(r'PlayerScoresCheck.csv', index=False, header=psc_headers)
+    pd.DataFrame(playerandscores).to_csv(r'PlayerScores.csv', index=False)
 
 def addPlayerScoretoCSV():
-    pl = pd.read_csv('pl.csv').to_numpy()
+    pl = pd.read_csv(file).to_numpy()
     newarray = []
     for player in range(0, len(pl)):
         if (str(pl[player][11]) == 'nan') and (int(pl[player][15]) > 0):
             newarray.append(pl[player])
-    pd.DataFrame(newarray).to_csv("pl2_reduced.csv", index=False)
+    pd.DataFrame(newarray).to_csv("reduced_player_list.csv", index=False)
 
-    array = pd.read_csv('pl2_reduced.csv').to_numpy()
-    array2 = pd.read_csv('Aug92021PlayerScores.csv').to_numpy()
+    array = pd.read_csv("reduced_player_list.csv").to_numpy()
+    array2 = pd.read_csv('PlayerScores.csv').to_numpy()
     array = pd.DataFrame(array)
     array['DFS_Score'] = ""
     array = array.to_numpy()
@@ -112,10 +160,10 @@ def addPlayerScoretoCSV():
         for j in range(0, len(array2)):
             if array[i][3] == array2[j][0]:
                 array[i][-1] = array2[j][1]
-    pd.DataFrame(array).to_csv("pl3.csv", index=False)  # THIS IS FOR PERMUTATION TESTING
+    pd.DataFrame(array).to_csv("player_list_with_scores.csv", index=False)  # THIS IS FOR PERMUTATION TESTING
 
 def editCombinationsWITHPlayerScores():
-    array = pd.read_csv('pl3.csv').to_numpy()
+    array = pd.read_csv("player_list_with_scores.csv").to_numpy()
     newarray = []
     for player in range(0, len(array)):
         if (str(array[player][11]) == 'nan') and (int(array[player][15]) > 0):
@@ -167,12 +215,24 @@ def editCombinationsWITHPlayerScores():
         predictedDFSScore = 0
         actualDFSScore = 0
 
-    permutationsarr.sort(key=lambda x: x[7], reverse=True)
-    pd.DataFrame(permutationsarr).to_csv("permsWScores.csv", index=False, header=headers)  # THIS IS FOR PERMUTATION TESTING
+    newpermutationsarr = []  # removes the repition of every six entries being the same
+    for i in range(6, len(permutationsarr) + 6):
+        if i % 6 == 0:  # every sixth and lineup cost above 30000
+            newpermutationsarr.append(permutationsarr[i - 6])
+    newpermutationsarr.sort(key=lambda x: x[7], reverse=True)
+    pd.DataFrame(newpermutationsarr).to_csv("permutations_with_scores.csv", index=False, header=headers)
+
+
+    ###ADDING PERCENTILES
+    x = pd.read_csv("permutations_with_scores.csv")
+    x['Predicted DFS Score Percentile Rank'] = x.PredictedDFSScore.rank(pct=True)
+    x['Actual DFS Score Percentile Rank'] = x.ActualDFSScore.rank(pct=True)
+    headers = ['Name1', 'Name2', 'Name3', 'Name4', 'Name5', 'SalaryTotal', 'PredictedDFSScore', 'ActualDFSScore','Predicted DFS Score Percentile Rank','Actual DFS Score Percentile Rank']
+    pd.DataFrame(x).to_csv("permutations_with_scores_and_percentiles.csv", index=False, header=headers)
 
 def seabornScatterplot():
     #['Name1','Name2','Name3','Name4','Name5','SalaryTotal','PredictedDFSScore','ActualDFSScore']
-    array = pd.read_csv('permsWScores.csv')
+    array = pd.read_csv(file+"permutations_with_scores.csv")
     sns.scatterplot(array['PredictedDFSScore'], array['ActualDFSScore']).set(title="Predicted DFS Score vs. Actual DFS Score Outcome")
     plt.show()
 
